6c4c756cad4482b4a209ee601ac0186de75603c1
User <User@LAPTOP-D03V7GQD>
2023-08-29 00:01:34 +0800

    commit2

---
+++ uppuma/src/uppuma/Test.java
@@ -0,0 +1,10 @@
+package uppuma;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		System.out.print("Hello! ...");
+		System.out.print("Java! ...");
+		
+	}
+}
---
+++ uppuma/src/uppuma/Test1.java
@@ -0,0 +1,18 @@
+package uppuma;
+
+public class Test1 {
+
+	
+	public static void main(String[] args) {
+
+		String hungry ="Yes";
+		if (hungry=="yes")
+			System.out.println("Eat Something.......");
+
+		else {
+			System.out.println("Do Work.......");
+
+
+	}
+}
+}
